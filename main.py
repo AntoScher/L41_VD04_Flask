066cfe84243dec9d9770d8f63e661f0ec2c0db87
AntoScher <asc8er@gmail.com>
2024-12-11 21:14:30 +0300

    home _work

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")
 
-def hello_world():
-    return render_template("index.html") # Внутри () пишем название html-файла в кавычках
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
