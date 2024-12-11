a989877593c8cd99b4320b7601dcc916e7fa80ec
AntoScher <asc8er@gmail.com>
2024-12-11 18:08:48 +0300

    next1

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
+
 def hello_world():
-    return "Hello World!"
-@app.route("/new/")
-
-
-@app.route("/newpage/")
-
-@app.route("/новаястраница/")
-def new():
-    return "new page"
-
+    return render_template() # Внутри () пишем название html-файла в кавычках
 
 
 if __name__ == "__main__":
     app.run()
+"""@app.route("/<name>/")
+def hello_world(name="незнакомец"):
+    return f"Hello, {name}"
+@app.route("/newpage/")
+@app.route("/новаястраница/")
+def new():    return "new page"
 
+@app.route("/<password>/")
+def hello_world(password=None):
+    if password == "1234":
+        return f"Доступ разрешён"
+    else:
+        return f"Доступ запрещён"
+"""
