eeaf079348c93b2c353c76981f4b4b24e940a40a
AntoScher <asc8er@gmail.com>
2024-12-12 11:30:20 +0300

    срф
    change1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #def index():    return render_template('index.html')
 
 def index(): # Чтение содержимого файла index_receipt.html
- with open(os.path.join(app.static_folder, 'index_receipt.html'), 'r', encoding='utf-8') as file:
+ with open(os.path.join(os.path.dirname(__file__), 'static', 'index_receipt.html'), 'r', encoding='utf-8') as file:
      receipt_content = file.read()
      return render_template('index.html', receipt_content=receipt_content)
 
