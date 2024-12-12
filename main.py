905d92106ef5b57cde99ae27525d56b0b9adc795
AntoScher <asc8er@gmail.com>
2024-12-12 10:21:01 +0300

    main table

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+#def index():    return render_template('index.html')
+
+def index(): # Чтение содержимого файла index_receipt.html
+ with open(os.path.join(app.static_folder, 'index_receipt.html'), 'r', encoding='utf-8') as file:
+     receipt_content = file.read()
+     return render_template('index.html', receipt_content=receipt_content)
 
 @app.route('/blog')
 def blog():
